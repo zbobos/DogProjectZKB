2f1d54d57750907ce8804716fa925fe78e3bb15f
zbobos <zbobos@localhost>
2016-03-16 16:11:08 -0400

    most recent commit GoodDog

---
+++ DogZKB/src/edu/saintjoe/cs/zbobos/DogZKB/PoorDog.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.zbobos.DogZKB;
+
+public class PoorDog {
+	
+	private int size;
+	private String name;
+	
+	public int getSize() {
+		return size;
+	}
+	
+}
---
+++ DogZKB/src/edu/saintjoe/cs/zbobos/DogZKB/PoorDogTestDrive.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.zbobos.DogZKB;
+
+public class PoorDogTestDrive {
+
+	public static void main(String[] args) {
+		PoorDog one = new PoorDog();
+		System.out.println(" Dog size is "+ one.getSize());
+		System.out.println(" Dog name is "+ one.getName());
+	}
+
+}
+
